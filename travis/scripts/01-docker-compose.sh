96bb1ce1a9d5976b18463b96cbdf9c8823bbe347
Bernd van der Veen <bernd@thehyve.nl>
2017-02-11 13:12:36 +0100

    debug

--- travis/scripts/01-docker-compose.sh
+++ travis/scripts/01-docker-compose.sh
@@ -4,8 +4,6 @@ set -e
 #-------------------------------------------------------------------------------
 # Start docker container
 #-------------------------------------------------------------------------------
-echo "$PODIUM_BASE"
-ls -la "$PODIUM_BASE"
 cd "$PODIUM_BASE"/podium-gateway
 if [ -a src/main/docker/podium-registry.yml ]; then
     docker-compose -f src/main/docker/podium-registry.yml up -d
