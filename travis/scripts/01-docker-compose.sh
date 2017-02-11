07b9225925b12a65781d3984b4ae79301c49a9ae
Bernd van der Veen <bernd@thehyve.nl>
2017-02-11 19:08:32 +0100

    Postgres port

--- travis/scripts/01-docker-compose.sh
+++ travis/scripts/01-docker-compose.sh
@@ -9,7 +9,7 @@ if [ -a src/main/docker/podium-registry.yml ]; then
     docker-compose -f src/main/docker/podium-registry.yml up -d
 fi
 if [ -a src/main/docker/postgresql.yml ]; then
-    docker-compose -f src/main/docker/postgresql.yml up -d
+    docker-compose -f src/main/docker/postgresql.yml -p localhost:5432 up -d
 fi
 if [ -a src/main/docker/elasticsearch.yml ]; then
     docker-compose -f src/main/docker/elasticsearch.yml up -d
