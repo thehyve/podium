6b3b97eed1368017fa3a550535422c0097b71891
Bernd van der Veen <bernd@thehyve.nl>
2017-02-12 13:42:00 +0100

    quiet mode maven

--- travis/scripts/03-tests.sh
+++ travis/scripts/03-tests.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 #-------------------------------------------------------------------------------
 # Check Javadoc generation for UAA and gateway
 #-------------------------------------------------------------------------------
 cd "$PODIUM_BASE"/podium-uaa
-mvn javadoc:javadoc
+mvn -q javadoc:javadoc
 cd "$PODIUM_BASE"/podium-gateway
-mvn javadoc:javadoc
+mvn -q javadoc:javadoc
 
 mkdir -p "$PODIUM_BASE"/podium-gateway/src/test/features
 
