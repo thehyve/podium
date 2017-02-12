2f7fbb705aaf5cc5ebd70c65550d21803b61a38d
Bernd van der Veen <bernd@thehyve.nl>
2017-02-12 14:38:04 +0100

    Maven quiet and batch mode

--- travis/scripts/02-prepare-databases.sh
+++ travis/scripts/02-prepare-databases.sh
@@ -4,7 +4,9 @@ set -e
 #-------------------------------------------------------------------------------
 # Prepare databases
 #-------------------------------------------------------------------------------
+export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cl‌​i.transfer.Slf4jMave‌​nTransferListener=wa‌​rn"
+
 cd "$PODIUM_BASE"/podium-uaa
-mvn liquibase:update
+mvn -q ${MAVEN_OPTS} liquibase:update
 cd "$PODIUM_BASE"/podium-gateway
-mvn liquibase:update
+mvn -q ${MAVEN_OPTS} liquibase:update
