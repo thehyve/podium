b35f751f748bb2d2358af107d01323a6beaf7dd0
Bernd van der Veen <bernd@thehyve.nl>
2017-02-12 14:51:37 +0100

    fish

--- travis/scripts/02-prepare-databases.sh
+++ travis/scripts/02-prepare-databases.sh
@@ -4,9 +4,7 @@ set -e
 #-------------------------------------------------------------------------------
 # Prepare databases
 #-------------------------------------------------------------------------------
-export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cl‌​i.transfer.Slf4jMave‌​nTransferListener=wa‌​rn"
-
 cd "$PODIUM_BASE"/podium-uaa
-mvn -q ${MAVEN_OPTS} liquibase:update
+mvn -q "$MAVEN_OPTS" liquibase:update
 cd "$PODIUM_BASE"/podium-gateway
-mvn -q ${MAVEN_OPTS} liquibase:update
+mvn -q "$MAVEN_OPTS" liquibase:update
--- travis/scripts/03-tests.sh
+++ travis/scripts/03-tests.sh
@@ -5,9 +5,9 @@ set -e
 # Check Javadoc generation for UAA and gateway
 #-------------------------------------------------------------------------------
 cd "$PODIUM_BASE"/podium-uaa
-mvn javadoc:javadoc
+mvn -q "$MAVEN_OPTS" javadoc:javadoc
 cd "$PODIUM_BASE"/podium-gateway
-mvn javadoc:javadoc
+mvn -q "$MAVEN_OPTS" javadoc:javadoc
 
 mkdir -p "$PODIUM_BASE"/podium-gateway/src/test/features
 
@@ -15,12 +15,12 @@ mkdir -p "$PODIUM_BASE"/podium-gateway/src/test/features
 # Launch UAA tests
 #-------------------------------------------------------------------------------
 cd "$PODIUM_BASE"/podium-uaa
-mvn test
+mvn -q "$MAVEN_OPTS" test
 #-------------------------------------------------------------------------------
 # Launch gateway tests
 #-------------------------------------------------------------------------------
 cd "$PODIUM_BASE"/podium-gateway
-mvn test \
+mvn  -q "$MAVEN_OPTS" test \
     -Dlogging.level.org.bbmri.podium.sample=ERROR \
     -Dlogging.level.org.bbmri.podium.travis=ERROR
 
