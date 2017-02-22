650f975eed54f82606e5ca13506c9dc2a299b196
Riza Nugraha <rnugraha@gmail.com>
2017-02-22 22:06:37 +0100

    :fish:

--- podium-gateway/src/main/webapp/app/layouts/layout-routing.module.ts
+++ podium-gateway/src/main/webapp/app/layouts/layout-routing.module.ts
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2017. The Hyve and respective contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  *
  * See the file LICENSE in the root of this repository.
@@ -14,8 +14,8 @@ import { RouterModule, Routes, Resolve } from '@angular/router';
 import { homeRoute } from '../home';
 import { navbarRoute } from '../app.route';
 import { errorRoute } from './';
-import {dashboardRoute} from '../dashboard/dashboard.route';
-import {completedRoute} from "./completed/completed.route";
+import { dashboardRoute } from '../dashboard/dashboard.route';
+import { completedRoute } from './completed/completed.route';
 
 let LAYOUT_ROUTES = [
     homeRoute,
@@ -26,11 +26,12 @@ let LAYOUT_ROUTES = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(LAYOUT_ROUTES, { useHash: true })
-  ],
-  exports: [
-    RouterModule
-  ]
+    imports: [
+        RouterModule.forRoot(LAYOUT_ROUTES, {useHash: true})
+    ],
+    exports: [
+        RouterModule
+    ]
 })
-export class LayoutRoutingModule {}
+export class LayoutRoutingModule {
+}
