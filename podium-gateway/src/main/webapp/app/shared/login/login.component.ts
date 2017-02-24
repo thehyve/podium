4f7b71ce633ac7b9d0cbb8212157ba1189328cbd
Riza Nugraha <rnugraha@gmail.com>
2017-02-24 11:45:26 +0100

    Changed home route in navbar to dashboard

--- podium-gateway/src/main/webapp/app/shared/login/login.component.ts
+++ podium-gateway/src/main/webapp/app/shared/login/login.component.ts
@@ -59,6 +59,7 @@ export class  PodiumLoginComponent implements OnInit, AfterViewInit {
     }
 
     login () {
+        console.log('asdads')
         this.loginService.login({
             username: this.username,
             password: this.password,
