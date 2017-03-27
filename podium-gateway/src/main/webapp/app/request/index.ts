131e8b80e66ef4389da541b46db306ed09a27995
Riza Nugraha <rnugraha@gmail.com>
2017-03-27 20:08:07 +0200

    Add new route for editing a request

--- podium-gateway/src/main/webapp/app/request/index.ts
+++ podium-gateway/src/main/webapp/app/request/index.ts
@@ -11,6 +11,7 @@
 export * from './form/request-form.component';
 export * from './form/request-form-submit-dialog.component';
 export * from './form/request-form.route';
+export * from './form/request-form-edit.route';
 export * from './form/request-form.service';
 export * from './overview/request-overview.component';
 export * from './overview/request-overview.route';
