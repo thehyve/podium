2e1125623f5cfbcca8592e9be781b4fc61e3ee5d
Bernd van der Veen <bernd@thehyve.nl>
2017-04-03 13:17:23 +0200

    Remove import of ng-bootstrap from shared lib

--- podium-gateway/src/main/webapp/app/shared/shared-libs.module.ts
+++ podium-gateway/src/main/webapp/app/shared/shared-libs.module.ts
@@ -12,7 +12,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgJhipsterModule } from 'ng-jhipster';
 import { InfiniteScrollModule } from 'angular2-infinite-scroll';
 import { PodiumUploadModule } from './upload/upload.module';
