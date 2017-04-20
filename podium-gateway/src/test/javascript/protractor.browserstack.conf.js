a6e403cae37b1b7bfa8c4ea42cae333b94d7e76d
barteld klasens <barteld@thehyve.nl>
2017-04-20 12:09:48 +0200

    set env vars for protractor

--- podium-gateway/src/test/javascript/protractor.browserstack.conf.js
+++ podium-gateway/src/test/javascript/protractor.browserstack.conf.js
@@ -26,15 +26,30 @@ exports.config = {
         'browserstack.local': true
     },
 
+    // Code to start browserstack local before start of test
+    beforeLaunch: function(){
+        console.log("Connecting local");
+        return new Promise(function(resolve, reject){
+            exports.bs_local = new browserstack.Local();
+            exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, function(error) {
+                if (error) return reject(error);
+                console.log('Connected. Now testing...');
+
+                resolve();
+            });
+        });
+    },
+
+    // Code to stop browserstack local after end of test
+    afterLaunch: function(){
+        return new Promise(function(resolve, reject){
+            exports.bs_local.stop(resolve);
+        });
+    },
+
     specs: [
         './e2e/features/*.feature'
     ],
-
-    // capabilities: {
-    //     'browserName': 'chrome',
-    // },
-
-    // directConnect: true,
 
     baseUrl: 'http://localhost:8080/',
 
