a93b65815a15cc74275a0768e5ecdc6c9bcaa8ee
Gijs Kant <gijs@thehyve.nl>
2017-05-02 18:17:33 +0200

    Request queries based on organisation or requester role. (WIP)

--- podium-gateway/src/main/java/nl/thehyve/podium/repository/RequestRepository.java
+++ podium-gateway/src/main/java/nl/thehyve/podium/repository/RequestRepository.java
@@ -7,12 +7,16 @@
 
 package nl.thehyve.podium.repository;
 
+import nl.thehyve.podium.common.enumeration.RequestReviewStatus;
 import nl.thehyve.podium.common.enumeration.RequestStatus;
 import nl.thehyve.podium.domain.Request;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -24,6 +28,24 @@ public interface RequestRepository extends JpaRepository<Request,Long> {
     Request findOneByUuid(UUID requestUuid);
 
     Page<Request> findAllByRequesterAndStatus(UUID requesterUuid, RequestStatus status, Pageable pageable);
+
+    @Query("select distinct r from Request r" +
+        " join r.organisations o" +
+        " where r.status = :status" +
+        " and o in :organisations")
+    Page<Request> findAllByStatusAndOrganisations(
+        @Param("status") RequestStatus status,
+        @Param("organisations") Set<UUID> organisations,
+        Pageable pageable);
+
+    @Query("select from Request r" +
+        " join r.organisations o" +
+        " where r.requestReviewProcess.status = :requestReviewStatus" +
+        " and o in :organisations")
+    Page<Request> findAllByRequestReviewStatusAndOrganisations(
+        @Param("requestReviewStatus") RequestReviewStatus requestReviewStatus,
+        @Param("organisations") Set<UUID> organisations,
+        Pageable pageable);
 
     Page<Request> findAllByRequester(UUID requesterUuid, Pageable pageable);
 
