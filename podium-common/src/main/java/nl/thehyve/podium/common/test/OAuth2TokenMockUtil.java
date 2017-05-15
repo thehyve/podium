a829703a7136c60f630e179a858d1316c4a060c5
berndvdveen <bernd@thehyve.nl>
2017-05-15 15:48:49 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    Message representation validation
    
    # This is the commit message #2:
    
    Fix double profile annotation

--- podium-common/src/main/java/nl/thehyve/podium/common/test/OAuth2TokenMockUtil.java
+++ podium-common/src/main/java/nl/thehyve/podium/common/test/OAuth2TokenMockUtil.java
@@ -37,7 +37,6 @@ import static org.mockito.BDDMockito.given;
 /**
  * A bean providing simple mocking of OAuth2 access tokens for security integration tests.
  */
-@Profile("test")
 @Component
 @Profile("test")
 public class OAuth2TokenMockUtil {
---
+++ podium-gateway/src/main/webapp/app/shared/data-loader/data-loader.component.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017. The Hyve and respective contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * See the file LICENSE in the root of this repository.
+ *
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pdm-data-loader',
+    templateUrl: './data-loader.component.html',
+    styleUrls: ['data-loader.scss']
+})
+
+export class DataLoaderComponent {
+
+}
