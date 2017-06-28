f8100f95358d9b45252d4871a32f6691092a00e7
Riza Nugraha <rnugraha@gmail.com>
2017-06-28 12:59:52 +0200

    Set linked request flag to false upon creation

--- podium-gateway/src/main/java/nl/thehyve/podium/service/DraftService.java
+++ podium-gateway/src/main/java/nl/thehyve/podium/service/DraftService.java
@@ -75,6 +75,7 @@ public class DraftService {
         request.setStatus(RequestStatus.Draft);
         request.setRequester(user.getUserUuid());
         RequestDetail requestDetail = new RequestDetail();
+        requestDetail.setCombinedRequest(false);
         requestDetail.setPrincipalInvestigator(new PrincipalInvestigator());
         request.setRequestDetail(requestDetail);
         requestRepository.save(request);
