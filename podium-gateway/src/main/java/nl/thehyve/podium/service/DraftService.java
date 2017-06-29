1c49a5b11af4e0ea4edfc5960518afbb2c4cecb9
berndvdveen <bernd@thehyve.nl>
2017-06-29 10:52:14 +0200

    import EntityManager

--- podium-gateway/src/main/java/nl/thehyve/podium/service/DraftService.java
+++ podium-gateway/src/main/java/nl/thehyve/podium/service/DraftService.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.*;
 import java.util.stream.Collectors;
 
