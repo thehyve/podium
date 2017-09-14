5cb66cc9c9fc7caa6976ebab68115d991b2c6824
Frank Bijlsma <fbijlsma@gmail.com>
2017-09-14 15:30:32 +0200

    error small refactor

--- podium-gateway/src/main/java/nl/thehyve/podium/service/RequestService.java
+++ podium-gateway/src/main/java/nl/thehyve/podium/service/RequestService.java
@@ -599,6 +599,9 @@ public class RequestService {
          List<Map<String, String>> missingOrgUUIDS = new ArrayList<>();
 
          for (Map<String, String> collection : collections) {
+             Map<String, String> error = new HashMap<>();
+             error.put("organisationId", collection.get("biobankID"));
+
              try {
                  UUID biobankID = UUID.fromString(collection.get("biobankID"));
                  log.debug("Checking for organization", biobankID);
@@ -609,20 +612,14 @@ public class RequestService {
                  if(organisationRepresentation.getActivated()){
                      organisations.add(organisationRepresentation);
                  } else {
-                     Map<String, String> error = new HashMap<>();
-                     error.put("organisationId", collection.get("biobankID"));
                      error.put("errorMessage", "Organisation linked is inactive");
                      missingOrgUUIDS.add(error);
                  }
              } catch (IllegalArgumentException e) {
-                 Map<String, String> error = new HashMap<>();
-                 error.put("organisationId", collection.get("biobankID"));
                  error.put("errorMessage", e.getMessage());
                  missingOrgUUIDS.add(error);
 
              } catch (HystrixRuntimeException e) {
-                 Map<String, String> error = new HashMap<>();
-                 error.put("organisationId", collection.get("biobankID"));
                  error.put("errorMessage", "Cannot find an organization for the given id: " +
                      collection.get("biobankID"));
                  missingOrgUUIDS.add(error);
