26320178ceff9940f7b8b416451f7d0078712599
aykl <alexejkhl@gmail.com>
2021-04-06 13:34:28 +0000

    Main Component moved

--- podium-gateway/src/main/webapp/app/layouts/main/main.component.ts
+++ podium-gateway/src/main/webapp/app/layouts/main/main.component.ts
@@ -9,8 +9,8 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
-import { JhiLanguageHelper } from '../shared';
-import { AccountService } from '../shared/auth/account.service';
+import { JhiLanguageHelper } from '../../shared';
+import { AccountService } from '../../shared/auth/account.service';
 
 @Component({
     selector: 'pdm-main',
