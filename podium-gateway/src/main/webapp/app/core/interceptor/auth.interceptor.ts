f45f9d60cef16632fc047e5615eb63771742f268
aykl <alexejkhl@gmail.com>
2021-04-22 13:24:59 +0200

    Auth Interceptor updated

--- podium-gateway/src/main/webapp/app/core/interceptor/auth.interceptor.ts
+++ podium-gateway/src/main/webapp/app/core/interceptor/auth.interceptor.ts
@@ -7,30 +7,27 @@
  * See the file LICENSE in the root of this repository.
  *
  */
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RequestOptionsArgs, Response } from '@angular/http';
-import { JhiHttpInterceptor } from 'ng-jhipster';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 
-export class AuthInterceptor extends JhiHttpInterceptor {
-
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
     constructor(
         private localStorage: LocalStorageService,
         private sessionStorage: SessionStorageService
-    ) {
-        super();
-    }
+    ) { }
 
-    requestIntercept(options?: RequestOptionsArgs): RequestOptionsArgs {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let token = this.localStorage.retrieve('authenticationToken') || this.sessionStorage.retrieve('authenticationToken');
-        if (!!token) {
-            options.headers.append('Authorization', 'Bearer ' + token);
+        if (token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
         }
-        return options;
+        return next.handle(request);
     }
-
-    responseIntercept(observable: Observable<Response>): Observable<Response> {
-        return observable; // by pass
-    }
-
 }
