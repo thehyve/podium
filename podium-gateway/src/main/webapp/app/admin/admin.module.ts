0bcf061f377afedac3cd9f9cfbc246331093bfa3
aykl <alexejkhl@gmail.com>
2021-04-25 14:32:20 +0200

    Imports updated

--- podium-gateway/src/main/webapp/app/admin/admin.module.ts
+++ podium-gateway/src/main/webapp/app/admin/admin.module.ts
@@ -11,17 +11,15 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PodiumGatewaySharedModule } from '../shared/shared.module';
 import { adminRoute } from './admin-routing.module';
-import {
-    AuditsComponent,
-    LogsComponent,
-    PdmMetricsMonitoringModalComponent,
-    PdmMetricsMonitoringComponent,
-    PdmHealthModalComponent,
-    PdmHealthCheckComponent,
-    PdmConfigurationComponent,
-    PdmDocsComponent,
-    PdmGatewayComponent,
-} from './';
+import { AuditsComponent } from './audits/audits.component';
+import { LogsComponent } from './logs/logs.component';
+import { PdmMetricsMonitoringModalComponent } from './metrics/metrics-modal.component';
+import { PdmMetricsMonitoringComponent } from './metrics/metrics.component';
+import { PdmHealthModalComponent } from './health/health-modal.component';
+import { PdmHealthCheckComponent } from './health/health.component';
+import { PdmConfigurationComponent } from './configuration/configuration.component';
+import { PdmDocsComponent } from './docs/docs.component';
+import { PdmGatewayComponent } from './gateway/gateway.component';
 import { PdmElasticsearchComponent } from './elasticsearch/elasticsearch.component';
 import { PdmElasticsearchModalComponent } from './elasticsearch/elasticsearch-modal.component';
 
