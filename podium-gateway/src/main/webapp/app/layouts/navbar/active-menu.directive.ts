8175848d3e4bfe4bf5c653c5de4fbd1dd74b7353
aykl <alexejkhl@gmail.com>
2021-04-26 09:43:06 +0200

    ActiveMenuDirective updated

--- podium-gateway/src/main/webapp/app/layouts/navbar/active-menu.directive.ts
+++ podium-gateway/src/main/webapp/app/layouts/navbar/active-menu.directive.ts
@@ -7,7 +7,7 @@
  * See the file LICENSE in the root of this repository.
  *
  */
-import { Directive, OnInit, ElementRef, Renderer, Input } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, Input } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Directive({
@@ -16,7 +16,7 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 export class ActiveMenuDirective implements OnInit {
     @Input() pdmActiveMenu: string;
 
-    constructor(private el: ElementRef, private renderer: Renderer, private translateService: TranslateService) { }
+    constructor(private el: ElementRef, private renderer: Renderer2, private translateService: TranslateService) { }
 
     ngOnInit() {
         this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
@@ -25,11 +25,11 @@ export class ActiveMenuDirective implements OnInit {
         this.updateActiveFlag(this.translateService.currentLang);
     }
 
-    updateActiveFlag(selectedLanguage) {
+    updateActiveFlag(selectedLanguage: string) {
         if (this.pdmActiveMenu === selectedLanguage) {
-            this.renderer.setElementClass(this.el.nativeElement, 'active', true);
+            this.renderer.addClass(this.el.nativeElement, 'active');
         } else {
-            this.renderer.setElementClass(this.el.nativeElement, 'active', false);
+            this.renderer.removeClass(this.el.nativeElement, 'active');
         }
     }
 }
