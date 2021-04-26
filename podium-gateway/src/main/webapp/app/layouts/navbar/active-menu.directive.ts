6f6c22677d01a59feb8218140c9f89f9d5e97b7c
aykl <alexejkhl@gmail.com>
2021-04-26 09:40:58 +0200

    Indentation fix

--- podium-gateway/src/main/webapp/app/layouts/navbar/active-menu.directive.ts
+++ podium-gateway/src/main/webapp/app/layouts/navbar/active-menu.directive.ts
@@ -16,20 +16,20 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 export class ActiveMenuDirective implements OnInit {
     @Input() pdmActiveMenu: string;
 
-    constructor(private el: ElementRef, private renderer: Renderer, private translateService: TranslateService) {}
+    constructor(private el: ElementRef, private renderer: Renderer, private translateService: TranslateService) { }
 
     ngOnInit() {
-      this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
-         this.updateActiveFlag(event.lang);
-      });
-      this.updateActiveFlag(this.translateService.currentLang);
+        this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+            this.updateActiveFlag(event.lang);
+        });
+        this.updateActiveFlag(this.translateService.currentLang);
     }
 
     updateActiveFlag(selectedLanguage) {
-      if (this.pdmActiveMenu === selectedLanguage) {
-          this.renderer.setElementClass(this.el.nativeElement, 'active', true);
-      } else {
-          this.renderer.setElementClass(this.el.nativeElement, 'active', false);
-      }
+        if (this.pdmActiveMenu === selectedLanguage) {
+            this.renderer.setElementClass(this.el.nativeElement, 'active', true);
+        } else {
+            this.renderer.setElementClass(this.el.nativeElement, 'active', false);
+        }
     }
 }
