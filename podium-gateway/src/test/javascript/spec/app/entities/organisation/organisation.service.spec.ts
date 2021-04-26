7a019e9ceda02483f6886f4b7d744cbd032de104
aykl <alexejkhl@gmail.com>
2021-04-26 10:36:50 +0200

    Obsolete Test removed

--- podium-gateway/src/test/javascript/spec/app/entities/organisation/organisation.service.spec.ts
+++ podium-gateway/src/test/javascript/spec/app/entities/organisation/organisation.service.spec.ts
@@ -34,52 +34,6 @@ describe('OrganisationService (Mocked)', () => {
             })
     ));
 
-    describe('findAll', () => {
-        const mockResponse = [ {
-            "id" : 1,
-            "uuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a",
-            "name" : "International variable name bank",
-            "shortName" : "VarnameBank",
-            "activated" : true,
-            "organisationUuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a"
-        }, {
-            "id" : 1000,
-            "uuid" : "549d67f8-7720-423a-ada9-bea83760e06a",
-            "name" : "International VarnameBank2",
-            "shortName" : "VarnameBank2",
-            "activated" : false,
-            "organisationUuid" : "549d67f8-7720-423a-ada9-bea83760e06a"
-        }];
-
-        it('should parse response', waitForAsync(inject(
-            [OrganisationService, MockBackend], (service, mockBackend) => {
-
-                mockBackend.connections.subscribe(conn => {
-                    conn.mockRespond(new Response(new ResponseOptions({body: JSON.stringify(mockResponse)})));
-                });
-
-                const result = service.findAll();
-
-                result.subscribe(res => {
-                    expect(res).toEqual([ {
-                        "id" : 1,
-                        "uuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a",
-                        "name" : "International variable name bank",
-                        "shortName" : "VarnameBank",
-                        "activated" : true,
-                        "organisationUuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a"
-                    }, {
-                        "id" : 1000,
-                        "uuid" : "549d67f8-7720-423a-ada9-bea83760e06a",
-                        "name" : "International VarnameBank2",
-                        "shortName" : "VarnameBank2",
-                        "activated" : false,
-                        "organisationUuid" : "549d67f8-7720-423a-ada9-bea83760e06a"
-                    }]);
-                });
-            })));
-    });
-
     describe('jsonArrayToOrganisations', () => {
 
         const _organisations = [new Organisation({
