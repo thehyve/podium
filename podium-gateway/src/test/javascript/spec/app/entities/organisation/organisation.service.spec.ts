af00de95e1d532255605a74d7e4a0d8839b3d58d
aykl <alexejkhl@gmail.com>
2021-04-26 10:38:54 +0200

    OrganisationService Test updated

--- podium-gateway/src/test/javascript/spec/app/entities/organisation/organisation.service.spec.ts
+++ podium-gateway/src/test/javascript/spec/app/entities/organisation/organisation.service.spec.ts
@@ -8,28 +8,22 @@
  *
  */
 import { waitForAsync, inject, TestBed } from '@angular/core/testing';
-import { BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import { PodiumTestModule } from '../../../test.module';
-import { OrganisationService } from '../../../../../../main/webapp/app/shared/organisation/organisation.service';
-import { Organisation } from '../../../../../../main/webapp/app/shared/organisation/organisation.model';
+import { OrganisationService } from './organisation.service';
+import { Organisation } from './organisation.model';
 
 
 describe('OrganisationService (Mocked)', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [PodiumTestModule],
             providers: [
                 OrganisationService,
-                MockBackend,
-                BaseRequestOptions
             ]
         });
     });
 
     it('should construct', waitForAsync(
-        inject([OrganisationService, MockBackend],
-            (service, mockBackend) => {
+        inject([OrganisationService],
+            (service) => {
                 expect(service).toBeDefined();
             })
     ));
@@ -41,23 +35,24 @@ describe('OrganisationService (Mocked)', () => {
             uuid: "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a",
             name: "International variable name bank",
             shortName: "VarnameBank",
-            activated : true,
+            activated: true,
             organisationUuid: "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a"
         })];
 
-        it('should convert array of json object to organisations', waitForAsync(inject([OrganisationService], (service) => {
-            let _jsonArray = [{
-                "id" : 1,
-                "uuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a",
-                "name" :  "International variable name bank",
-                "shortName" : "VarnameBank",
-                "activated" : true,
-                "organisationUuid" : "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a"
-            }];
-            let res = service.jsonArrayToOrganisations(_jsonArray);
+        it('should convert array of json object to organisations', waitForAsync(
+            inject([OrganisationService], (service) => {
+                let _jsonArray = [{
+                    "id": 1,
+                    "uuid": "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a",
+                    "name": "International variable name bank",
+                    "shortName": "VarnameBank",
+                    "activated": true,
+                    "organisationUuid": "12dd08b3-eb8b-476e-a0b3-716cb6b5df7a"
+                }];
+                let res = service.jsonArrayToOrganisations(_jsonArray);
 
-            expect(res).toEqual(_organisations);
-        })));
+                expect(res).toEqual(_organisations);
+            })));
 
     });
 });
