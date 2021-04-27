a582da940df6f2b10a73fd86dc663e1afbe9353d
aykl <alexejkhl@gmail.com>
2021-04-27 14:12:13 +0200

    Health Page updated

--- podium-gateway/src/main/webapp/app/admin/health/health.component.ts
+++ podium-gateway/src/main/webapp/app/admin/health/health.component.ts
@@ -1,70 +1,45 @@
-/*
- * Copyright (c) 2017. The Hyve and respective contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * See the file LICENSE in the root of this repository.
- *
- */
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { PdmHealthService } from './health.service';
-import { PdmHealthModalComponent } from './health-modal.component';
+
+import { HealthService } from './health.service';
+import { Health, HealthDetails, HealthStatus } from './health.model';
+import { HealthModalComponent } from './modal/health-modal.component';
 
 @Component({
-    selector: 'pdm-health',
-    templateUrl: './health.component.html',
+  selector: 'jhi-health',
+  templateUrl: './health.component.html',
 })
-export class PdmHealthCheckComponent implements OnInit {
-    healthData: any;
-    updatingHealth: boolean;
+export class HealthComponent implements OnInit {
+  health?: Health;
 
-    constructor(
-        private modalService: NgbModal,
-        private healthService: PdmHealthService
-    ) {
+  constructor(private modalService: NgbModal, private healthService: HealthService) {}
 
+  ngOnInit(): void {
+    this.refresh();
+  }
 
+  getBadgeClass(statusState: HealthStatus): string {
+    if (statusState === 'UP') {
+      return 'badge-success';
+    } else {
+      return 'badge-danger';
     }
+  }
 
-    ngOnInit() {
-        this.refresh();
-    }
-
-    baseName(name: string) {
-        return this.healthService.getBaseName(name);
-    }
-
-    getBadgeClass(statusState) {
-        if (statusState === 'UP') {
-            return 'badge-success';
-        } else {
-            return 'badge-danger';
+  refresh(): void {
+    this.healthService.checkHealth().subscribe(
+      health => (this.health = health),
+      (error: HttpErrorResponse) => {
+        if (error.status === 503) {
+          this.health = error.error;
         }
-    }
+      }
+    );
+  }
 
-    refresh() {
-        this.updatingHealth = true;
-
-        this.healthService.checkHealth().subscribe(health => {
-            this.healthData = this.healthService.transformHealthData(health);
-            this.updatingHealth = false;
-        });
-    }
-
-    showHealth(health: any) {
-        const modalRef  = this.modalService.open(PdmHealthModalComponent);
-        modalRef.componentInstance.currentHealth = health;
-        modalRef.result.then((result) => {
-            console.log(`Closed with: ${result}`);
-        }, (reason) => {
-            console.log(`Dismissed ${reason}`);
-        });
-    }
-
-    subSystemName(name: string) {
-        return this.healthService.getSubSystemName(name);
-    }
-
+  showHealth(health: { key: string; value: HealthDetails }): void {
+    const modalRef = this.modalService.open(HealthModalComponent);
+    modalRef.componentInstance.health = health;
+  }
 }
