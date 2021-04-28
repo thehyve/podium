85e8bfce93980c611aca21ca5e83da34c2acc0d6
aykl <alexejkhl@gmail.com>
2021-04-28 16:10:55 +0200

    Interceptor updated

--- podium-gateway/src/main/webapp/app/core/interceptor/error-handler.interceptor.ts
+++ podium-gateway/src/main/webapp/app/core/interceptor/error-handler.interceptor.ts
@@ -12,21 +12,21 @@ import { HttpInterceptor, HttpRequest, HttpErrorResponse, HttpHandler, HttpEvent
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-import { EventManager } from '../util/event-manager.service';
+import { EventManager, EventWithContent } from '../util/event-manager.service';
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
-  constructor(private eventManager: EventManager) {}
+    constructor(private eventManager: EventManager) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      tap({
-        error: (err: HttpErrorResponse) => {
-          if (!(err.status === 401 && (err.message === '' || err.url?.includes('api/account')))) {
-            this.eventManager.broadcast({ name: 'podiumGatewayApp.httpError', content: err });
-          }
-        },
-      })
-    );
-  }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(
+            tap({
+                error: (err: HttpErrorResponse) => {
+                    if (!(err.status === 401 && (err.message === '' || err.url?.includes('zapi/account')))) {
+                        this.eventManager.broadcast(new EventWithContent('podiumApp.httpError', err));
+                    }
+                },
+            })
+        );
+    }
 }
