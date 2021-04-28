7f5d3f5e0fa580baefd114be600f915301af2e10
aykl <alexejkhl@gmail.com>
2021-04-28 14:26:06 +0200

    Auth Interceptor updated

--- podium-gateway/src/main/webapp/app/core/interceptor/auth.interceptor.ts
+++ podium-gateway/src/main/webapp/app/core/interceptor/auth.interceptor.ts
@@ -12,15 +12,23 @@ import { Observable } from 'rxjs';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 
+import { ApplicationConfigService } from '../config/application-config.service';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     constructor(
         private localStorage: LocalStorageService,
-        private sessionStorage: SessionStorageService
+        private sessionStorage: SessionStorageService,
+        private applicationConfigService: ApplicationConfigService
     ) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let token = this.localStorage.retrieve('authenticationToken') || this.sessionStorage.retrieve('authenticationToken');
+        const serverApiUrl = this.applicationConfigService.getEndpointFor('');
+        if (!request.url || (request.url.startsWith('http') && !(serverApiUrl && request.url.startsWith(serverApiUrl)))) {
+            return next.handle(request);
+        }
+
+        const token: string | null = this.localStorage.retrieve('authenticationToken') ?? this.sessionStorage.retrieve('authenticationToken');
         if (token) {
             request = request.clone({
                 setHeaders: {
