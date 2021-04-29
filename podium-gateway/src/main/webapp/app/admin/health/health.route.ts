c08fc15f8907e9e09ca83db2ba661f0f0d3e7a48
aykl <alexejkhl@gmail.com>
2021-04-29 13:53:59 +0200

    JhiTranslate -> Translate

--- podium-gateway/src/main/webapp/app/admin/health/health.route.ts
+++ podium-gateway/src/main/webapp/app/admin/health/health.route.ts
@@ -1,11 +1,20 @@
+/*
+ * Copyright (c) 2017. The Hyve and respective contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * See the file LICENSE in the root of this repository.
+ *
+ */
 import { Route } from '@angular/router';
-
-import { HealthComponent } from './health.component';
+import { PdmHealthCheckComponent } from './health.component';
 
 export const healthRoute: Route = {
-  path: '',
-  component: HealthComponent,
-  data: {
-    pageTitle: 'Health Checks',
-  },
+    path: 'pdm-health',
+    component: PdmHealthCheckComponent,
+    data: {
+        pageTitle: 'health.title',
+        breadcrumb: 'podium health'
+    }
 };
