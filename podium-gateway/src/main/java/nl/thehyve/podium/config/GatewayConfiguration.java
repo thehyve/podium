083042d656145fb0e3d2c3caf28e5986509b3fb5
Gijs Kant <gijs@thehyve.nl>
2021-05-24 23:58:38 +0200

    Clean up unused rate limiting feature.

--- podium-gateway/src/main/java/nl/thehyve/podium/config/GatewayConfiguration.java
+++ podium-gateway/src/main/java/nl/thehyve/podium/config/GatewayConfiguration.java
@@ -7,13 +7,9 @@
 
 package nl.thehyve.podium.config;
 
-import com.datastax.driver.core.Session;
 import nl.thehyve.podium.common.config.PodiumProperties;
 import nl.thehyve.podium.gateway.accesscontrol.AccessControlFilter;
-import nl.thehyve.podium.gateway.ratelimiting.RateLimitingFilter;
-import nl.thehyve.podium.gateway.ratelimiting.RateLimitingRepository;
 import nl.thehyve.podium.gateway.responserewriting.SwaggerBasePathRewritingFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,40 +32,6 @@ public class GatewayConfiguration {
         @Bean
         public AccessControlFilter accessControlFilter(RouteLocator routeLocator, PodiumProperties podiumProperties){
             return new AccessControlFilter(routeLocator, podiumProperties);
-        }
-    }
-
-    /**
-     * Configures the Zuul filter that limits the number of API calls per user.
-     * <p>
-     * For this filter to work, you need to have:
-     * <ul>
-     * <li>A working Cassandra cluster
-     * <li>A schema with the Podium rate-limiting tables configured, using the
-     * "create_keyspace.cql" and "create_tables.cql" scripts from the
-     * "src/main/resources/config/cql" directory
-     * <li>Your cluster configured in your application-*.yml files, using the
-     * "spring.data.cassandra" keys
-     * </ul>
-     */
-    @Configuration
-    @ConditionalOnProperty("podium.gateway.rate-limiting.enabled")
-    public static class RateLimitingConfiguration {
-
-        private final PodiumProperties podiumProperties;
-
-        public RateLimitingConfiguration(PodiumProperties podiumProperties) {
-            this.podiumProperties = podiumProperties;
-        }
-
-        @Bean
-        public RateLimitingRepository rateLimitingRepository(Session session) {
-            return new RateLimitingRepository(session);
-        }
-
-        @Bean
-        public RateLimitingFilter rateLimitingFilter(RateLimitingRepository rateLimitingRepository) {
-            return new RateLimitingFilter(rateLimitingRepository, podiumProperties);
         }
     }
 }
